c1f0817d73965087e898159a904133a1ed29d3cb
campagno <campagno.eduardo@outlook.com>
2020-03-10 20:42:50 -0300

    Escribiendo Archivo txt

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const superagent = require('superagent');
+const fs = require('fs');
+
 
 function imprimirMuseos(error, respuesta) {
   if (error) {
@@ -11,12 +13,31 @@ function imprimirMuseos(error, respuesta) {
   console.log(`Se encontraron ${cantidad} museos.`);
   console.log(`El primer museo se llama ${museos[0].nombre}.`)
 }
+function terminar() {
+  console.log('Terminé de Leer')
+} 
 
 console.log('Antes de llamar a superagent')
+
+function respuesta(error) {
+  if(error) {
+    throw new Error('no se pudo escribir');
+  }
+  console.log('Leer archivo')
+  
+}
+
 
 superagent
   .get('https://www.cultura.gob.ar/api/v2.0/museos')
   .query({ format: 'json' })
-  .end(imprimirMuseos)
+  .end(escribirArchivo)
+
+function escribirArchivo(error, respuesta) {
+  const museos = respuesta.body.results;
+  fs.writeFile('museos.txt',museos[4].nombre, terminar);
+}
 
 console.log('Después de llamar a superagent')
+
+
